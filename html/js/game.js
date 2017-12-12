7871284883195c99accf69d2c8dab02d754723fd
ethorpo <32637416+ethorpo@users.noreply.github.com>
2017-12-12 12:39:06 +0000

    Add files via upload

--- html/js/game.js
+++ html/js/game.js
@@ -727,7 +727,7 @@ function start ()
 {
 	game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
 	
-		keyCount = 0;
+		keyCount = 8;
 		coinCount = 0;
 		specialCount = 0;
 	    cursors = game.input.keyboard.createCursorKeys();
